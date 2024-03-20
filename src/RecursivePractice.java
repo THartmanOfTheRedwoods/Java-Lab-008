9eb83fc320ab36813547f955031771c2304257c8
vathao777 <vathao777@gmail.com>
2024-03-20 13:31:29 -0700

    Finished Lab-008

--- src/RecursivePractice.java
+++ src/RecursivePractice.java
@@ -1,5 +1,17 @@
 public class RecursivePractice {
+    public static void Breakingdown(int numBottles) {
+        if (numBottles == 0) {
+            System.out.println("No bottles of beer on the wall,\n" +
+                    "no bottles of beer,\n" +
+                    "ya’ can’t take one down, ya’ can’t pass it around, ’cause there are no more bottles of beer on the wall!");
+        } else {
 
-    public static void main(String[] args) {
+            System.out.printf(numBottles+" bottles of beer on the wall,\n" +
+                    numBottles+" bottles of beer,\n" +
+                    " ya’ take one down, ya’ pass it around, %d bottles of beer on the wall.",(numBottles-1));
+            Breakingdown(numBottles - 1);
+
+        }
     }
+
 }
